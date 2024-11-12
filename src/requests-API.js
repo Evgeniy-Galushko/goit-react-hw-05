8d5c2884b51c19becf16e419d48f4bd5b79032aa
Galushko Evgeniy <yudginchik@gmail.com>
2024-11-12 12:17:08 +0200

    add

--- src/requests-API.js
+++ src/requests-API.js
@@ -51,3 +51,37 @@ export async function requestMoviesId(id) {
   console.log(savedQuery);
   return savedQuery;
 }
+
+export async function requestCast() {
+  const savedQuery = await axios.get("https://api.themoviedb.org/3/movie/", {
+    params: {
+      client_id: "f596add8eda90ab82908f758f22a39bc",
+      movie_id: ``,
+      language: "ru",
+    },
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTk2YWRkOGVkYTkwYWI4MjkwOGY3NThmMjJhMzliYyIsIm5iZiI6MTczMTA4OTczNy45MjQ5MTc1LCJzdWIiOiI2NzJlNTFiNmYwOTI3YWNkZTBkMWM5Y2YiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.W9suki6imrjDbtuCBdOyqxhQrIrn6x1y6BqZkly9lRw",
+      accept: "application/json",
+    },
+  });
+  console.log(savedQuery);
+  return savedQuery;
+}
+
+export async function requestReviews() {
+  const savedQuery = await axios.get("https://api.themoviedb.org/3/movie/", {
+    params: {
+      client_id: "f596add8eda90ab82908f758f22a39bc",
+      movie_id: ``,
+      language: "ru",
+    },
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTk2YWRkOGVkYTkwYWI4MjkwOGY3NThmMjJhMzliYyIsIm5iZiI6MTczMTA4OTczNy45MjQ5MTc1LCJzdWIiOiI2NzJlNTFiNmYwOTI3YWNkZTBkMWM5Y2YiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.W9suki6imrjDbtuCBdOyqxhQrIrn6x1y6BqZkly9lRw",
+      accept: "application/json",
+    },
+  });
+  console.log(savedQuery);
+  return savedQuery;
+}
